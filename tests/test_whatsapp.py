4e1801f9c08986ad66247877799c8c803d82106e
gonzalogacc <gonzalogacc@gmail.com>
2023-09-01 02:25:50 +0200

    Test fail on simple message

--- tests/test_whatsapp.py
+++ tests/test_whatsapp.py
@@ -3,7 +3,7 @@ from urllib import request
 import pytest
 from httpx import QueryParams
 
-from src.whatsapp.schemas import WhatsappMessage
+from src.whatsapp.schemas import WhatsappMessage, WrongMessageType
 from src.whatsapp.whatsapp import verify_webhook, WhatsappClient
 
 
@@ -35,3 +35,10 @@ def test_extract_latlong_from_message():
     assert coordinates.longitude == 2.1878051757812
     assert coordinates.latitude == 41.381469726562
 
+def test_extract_latlong_from_text_message():
+    # get a Location message and extract the coordinates with the function
+    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {'messaging_product': 'whatsapp', 'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'}, 'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [{'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgWM0VCMDcwOTVGNDA5ODBGMjREMkY1QQA=', 'timestamp': '1693437013', 'text': {'body': 'hola'}, 'type': 'text'}]}, 'field': 'messages'}]}]}
+
+    with pytest.raises(WrongMessageType) as error:
+        wac = WhatsappClient()
+        coordinates = wac.process_location_message(WhatsappMessage(**raw))
