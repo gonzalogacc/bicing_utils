92ca5b2de7efd36024a29ad8d63a4d0ba36b2cab
gonzalogacc <gonzalogacc@gmail.com>
2023-09-03 01:59:08 +0200

    File reformat

--- tests/test_whatsapp.py
+++ tests/test_whatsapp.py
@@ -9,16 +9,30 @@ from src.whatsapp.schemas import WhatsappMessageIN, WrongMessageType, WhatsappMe
 from src.whatsapp.whatsapp import WhatsappClient
 from tests.test_fixtures import whatsapp_test_API
 
+
 def test_webhook_object_SimpleMessage():
-    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {'messaging_product': 'whatsapp', 'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'}, 'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [{'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgWM0VCMDcwOTVGNDA5ODBGMjREMkY1QQA=', 'timestamp': '1693437013', 'text': {'body': 'hola'}, 'type': 'text'}]}, 'field': 'messages'}]}]}
+    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {
+        'messaging_product': 'whatsapp',
+        'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'},
+        'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [
+            {'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgWM0VCMDcwOTVGNDA5ODBGMjREMkY1QQA=',
+             'timestamp': '1693437013', 'text': {'body': 'hola'}, 'type': 'text'}]}, 'field': 'messages'}]}]}
     wo = WhatsappMessageIN(**raw)
     assert wo.entry[0].id == 104246805978102
 
+
 def test_webhook_object_LocationMessage():
-    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {'messaging_product': 'whatsapp', 'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'}, 'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [{'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgUM0FEMjcwQzBBRDVENDc5NDg4NzQA', 'timestamp': '1693442282', 'location': {'latitude': 41.381469726562, 'longitude': 2.1878051757812}, 'type': 'location'}]}, 'field': 'messages'}]}]}
+    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {
+        'messaging_product': 'whatsapp',
+        'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'},
+        'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [
+            {'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgUM0FEMjcwQzBBRDVENDc5NDg4NzQA',
+             'timestamp': '1693442282', 'location': {'latitude': 41.381469726562, 'longitude': 2.1878051757812},
+             'type': 'location'}]}, 'field': 'messages'}]}]}
     wo = WhatsappMessageIN(**raw)
     assert wo.entry[0].changes[0].value.messages[0].location.latitude == 41.381469726562
     assert wo.entry[0].changes[0].value.messages[0].location.longitude == 2.1878051757812
+
 
 def test_endpoint_verification():
     ## URL
@@ -28,22 +42,36 @@ def test_endpoint_verification():
     response = verify_webhook()
     pass
 
+
 def test_extract_latlong_from_message():
     # get a Location message and extract the coordinates with the function
-    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {'messaging_product': 'whatsapp', 'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'}, 'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [{'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgUM0FEMjcwQzBBRDVENDc5NDg4NzQA', 'timestamp': '1693442282', 'location': {'latitude': 41.381469726562, 'longitude': 2.1878051757812}, 'type': 'location'}]}, 'field': 'messages'}]}]}
+    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {
+        'messaging_product': 'whatsapp',
+        'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'},
+        'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [
+            {'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgUM0FEMjcwQzBBRDVENDc5NDg4NzQA',
+             'timestamp': '1693442282', 'location': {'latitude': 41.381469726562, 'longitude': 2.1878051757812},
+             'type': 'location'}]}, 'field': 'messages'}]}]}
     wac = WhatsappClient()
     coordinates = wac.process_location_message(WhatsappMessageIN(**raw))
 
     assert coordinates.longitude == 2.1878051757812
     assert coordinates.latitude == 41.381469726562
 
+
 def test_extract_latlong_from_text_message():
     # get a Location message and extract the coordinates with the function
-    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {'messaging_product': 'whatsapp', 'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'}, 'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [{'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgWM0VCMDcwOTVGNDA5ODBGMjREMkY1QQA=', 'timestamp': '1693437013', 'text': {'body': 'hola'}, 'type': 'text'}]}, 'field': 'messages'}]}]}
+    raw = {'object': 'whatsapp_business_account', 'entry': [{'id': '104246805978102', 'changes': [{'value': {
+        'messaging_product': 'whatsapp',
+        'metadata': {'display_phone_number': '34623508545', 'phone_number_id': '102033176202052'},
+        'contacts': [{'profile': {'name': 'Gonza'}, 'wa_id': '447472138610'}], 'messages': [
+            {'from': '447472138610', 'id': 'wamid.HBgMNDQ3NDcyMTM4NjEwFQIAEhgWM0VCMDcwOTVGNDA5ODBGMjREMkY1QQA=',
+             'timestamp': '1693437013', 'text': {'body': 'hola'}, 'type': 'text'}]}, 'field': 'messages'}]}]}
 
     with pytest.raises(WrongMessageType) as error:
         wac = WhatsappClient()
         coordinates = wac.process_location_message(WhatsappMessageIN(**raw))
+
 
 def test_upload_image_function_ok(whatsapp_test_API):
     media_resource = WhatsappMediaResource(
@@ -59,11 +87,12 @@ def test_upload_image_function_ok(whatsapp_test_API):
     print(response)
     assert response.id == 1363181297589232
 
+
 def test_send_whatsapp_message_OK(whatsapp_test_API):
     text_message = WhatsappMessageOUT(
-        to = "+447472138610",
-        type = WhatsappMediaTypeEnum.text,
-        text = WhatsappTextObject(body="test message")
+        to="+447472138610",
+        type=WhatsappMediaTypeEnum.text,
+        text=WhatsappTextObject(body="test message")
     )
     wc = WhatsappClient()
     wc._http_client = whatsapp_test_API
